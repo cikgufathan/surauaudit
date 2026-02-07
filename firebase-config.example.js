e25c933b7de669dcc787617142de8877ae67d31e
cikgufathan <apipsmu@gmail.com>
2026-02-07 23:40:14 +0800

    Create firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,12 @@
+// Salin fail ini sebagai `firebase-config.js` dan isi nilai projek Firebase anda.
+export const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+// ID koleksi Firestore untuk simpan transaksi.
+export const FIRESTORE_COLLECTION = "masjid_transactions";
