1758d086bb2375a45f5b666e2c20f0614b860076
cikgufathan <apipsmu@gmail.com>
2026-02-08 00:09:13 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// Isi konfigurasi Firebase anda untuk aktifkan cloud sync.
-// Jika dibiarkan kosong, aplikasi akan fallback ke localStorage sahaja.
+// firebase-config.js
+
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyB6tfV8GVktWRJYniXBxA97cIsJF9prmP8",
+  authDomain: "surau-audit.firebaseapp.com",
+  projectId: "surau-audit",
+  storageBucket: "surau-audit.firebasestorage.app",
+  messagingSenderId: "573509490725",
+  appId: "1:573509490725:web:15ab6efdfbc390db54c7cc"
 };
 
 export const FIRESTORE_COLLECTION = "masjid_transactions";
